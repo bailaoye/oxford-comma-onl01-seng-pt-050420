991b2d6738681a310ac9bf4ced77a62c0b87f2ea
Jeff Bai <j.bai@wustl.edu>
2020-05-14 09:25:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 def oxford_comma(array)
-  if 
-    array.join(", and")
-  else
-    array.join(", ")
-    i += 1
+  if array.length == 2
+    array[-2] << " and "
+    array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
   end
 end
+
+oxford_comma(array)
